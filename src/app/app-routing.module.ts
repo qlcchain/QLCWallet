e7528fabc3294a1e9e761ea713126c1561abc4cb
Goren G <yong.gu@qlink.mobi>
2018-07-19 12:23:11 +0800

    fix app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from "./welcome/welcome.component";
-import { ConfigureWalletComponent } from "./components/configure-wallet/configure-wallet.component";
-import { AccountsComponent } from "./components/accounts/accounts.component";
-import { SendComponent } from "./components/send/send.component";
-import { AddressBookComponent } from "./components/address-book/address-book.component";
-import { ReceiveComponent } from "./components/receive/receive.component";
-import { ManageWalletComponent } from "./components/manage-wallet/manage-wallet.component";
-import { ConfigureAppComponent } from "./components/configure-app/configure-app.component";
-import { AccountDetailsComponent } from "./components/account-details/account-details.component";
-import { TransactionDetailsComponent } from "./components/transaction-details/transaction-details.component";
-import { ImportWalletComponent } from "./components/import-wallet/import-wallet.component";
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ConfigureWalletComponent } from './components/configure-wallet/configure-wallet.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { SendComponent } from './components/send/send.component';
+import { AddressBookComponent } from './components/address-book/address-book.component';
+import { ReceiveComponent } from './components/receive/receive.component';
+import { ManageWalletComponent } from './components/manage-wallet/manage-wallet.component';
+import { ConfigureAppComponent } from './components/configure-app/configure-app.component';
+import { AccountDetailsComponent } from './components/account-details/account-details.component';
+import { TransactionDetailsComponent } from './components/transaction-details/transaction-details.component';
+import { ImportWalletComponent } from './components/import-wallet/import-wallet.component';
 import { ImportAddressBookComponent } from './components/import-address-book/import-address-book.component';
-import { RepresentativesComponent } from "./components/representatives/representatives.component";
+import { RepresentativesComponent } from './components/representatives/representatives.component';
 
 import { environment } from '../environments/environment';
-import { ManageRepresentativesComponent } from "./components/manage-representatives/manage-representatives.component";
+import { ManageRepresentativesComponent } from './components/manage-representatives/manage-representatives.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
