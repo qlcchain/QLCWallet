d0d73c22f3e0cbca0b48ea8d3f67fd115ab3e4ee
Goren G <yong.gu@qlink.mobi>
2019-01-04 22:45:29 +0800

    refactor: change api to json rpc
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,18 +3,27 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { NodeService } from './node.service';
 import { NGXLogger } from 'ngx-logger';
-
 @Injectable()
 export class ApiService {
 	rpcUrl = environment.apiUrl;
+	// buff = new SharedArrayBuffer(32);
+	// id = new Uint32Array(this.buff);
+	id = 1;
 
 	constructor(private http: HttpClient, private node: NodeService, private logger: NGXLogger) {
 		this.logger.debug(this.rpcUrl);
+		// this.id[0] = 1;
 	}
 
 	private async request(action, data): Promise<any> {
-		data.action = action;
-		// console.log('requesting: ' + action);
+		data.jsonrpc = '2.0';
+		data.method = action;
+		data.id = this.id;
+		this.id++;
+		// data.id = Atomics.load(this.id, 0);
+		console.log('requesting' + JSON.stringify(data));
+		// Atomics.add(this.id, 0, 1);
+
 		return await this.http
 			.post(this.rpcUrl, data)
 			.toPromise()
@@ -31,62 +40,50 @@ export class ApiService {
 	}
 
 	async accountsBalances(accounts: string[]): Promise<{ balances: any }> {
-		return await this.request('accounts_balances', { accounts });
+		return await this.request('qlcclassic_accountsBalances', { params: accounts });
 	}
 	async accountsFrontiers(accounts: string[]): Promise<{ frontiers: any; error?: string }> {
-		return await this.request('accounts_frontiers', { accounts });
+		return await this.request('qlcclassic_accountsFrontiers', { params: accounts });
 	}
 	async accountsPending(accounts: string[], count: number = 50): Promise<{ blocks: any }> {
-		return await this.request('accounts_pending', {
-			accounts,
-			count,
-			source: true
-		});
+		return await this.request('qlcclassic_accountsPending', { params: [accounts, count, true] });
 	}
 	async delegatorsCount(account: string): Promise<{ count: string }> {
 		return await this.request('delegators_count', { account });
 	}
 	async representativesOnline(): Promise<{ representatives: any }> {
-		return await this.request('representatives_online', {});
+		return await this.request('qlcclassic_representativesOnline', {});
 	}
 
 	async blocksInfo(blocks): Promise<{ blocks: any; error?: string }> {
-		return await this.request('blocks_info', {
-			hashes: blocks,
-			pending: true,
-			source: true
-		});
+		return await this.request('qlcclassic_blocksInfo', { params: [blocks, true, true] });
 	}
 	async blockCount(): Promise<{ count: number; unchecked: number }> {
-		return await this.request('block_count', {});
+		return await this.request('qlcclassic_blockCount', {});
 	}
 	async workGenerate(hash): Promise<{ work: string }> {
-		return await this.request('work_generate', { hash });
+		return await this.request('qlcclassic_workGenerate', { params: [hash] });
 	}
 	async process(block): Promise<{ hash: string; error?: string }> {
-		return await this.request('process', { block: JSON.stringify(block) });
+		return await this.request('qlcclassic_process', { params: [block] });
 	}
 	async accountHistory(account, count = 25, raw = false): Promise<{ history: any }> {
-		return await this.request('account_history_topn', { account, count, raw });
+		return await this.request('qlcclassic_accountHistoryTopn', { params: [account, count, raw] });
 	}
 	async accountInfo(account): Promise<any> {
-		return await this.request('account_info', {
-			account,
-			pending: true,
-			representative: true,
-			weight: true
-		});
+		return await this.request('qlcclassic_accountInfo', { params: [account, true, true, true] });
 	}
 	async validateAccountNumber(account): Promise<{ valid: '1' | '0' }> {
-		return await this.request('validate_account_number', { account });
+		return await this.request('qlcclassic_validateAccountNumber', { params: [account] });
 	}
 	async pending(account, count): Promise<any> {
-		return await this.request('pending', { account, count, source: true });
+		return await this.request('qlcclassic_pending', { params: [account, count, true] });
 	}
 	async tokens(): Promise<{ tokens: any; error?: string }> {
-		return await this.request('tokens', {});
+		return await this.request('qlcclassic_tokens', {});
 	}
 
+	// TODO: fix
 	async tokenByName(token_name): Promise<{ token_info: any }> {
 		const tokenRespone = await this.tokens();
 		const tokens = tokenRespone.tokens;
@@ -102,6 +99,7 @@ export class ApiService {
 		return token;
 	}
 
+	// TODO: fix
 	async accountInfoByToken(account, tokenHash): Promise<any> {
 		const account_infos = await this.accountInfo(account);
 		const token_accounts = account_infos.account_infos;
